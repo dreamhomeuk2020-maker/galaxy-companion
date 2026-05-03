f51d448b8e3cdf6e62a775bcc8cee155976a3acc
SWG-Skywalker <dreamhomeuk2020@gmail.com>
2026-05-03 19:25:31 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,19 @@
+const { scrapeGalaxy } = require("./src/scraper");
+const Cache = require("./models/Cache");
 
+async function run() {
+  const data = await scrapeGalaxy();
+
+  await Cache.findOneAndUpdate(
+    {},
+    { ...data, updatedAt: new Date() },
+    { upsert: true }
+  );
+
+  console.log("Galaxy data updated");
+}
+
+// every 10 minutes
+setInterval(run, 600000);
+
+run();
